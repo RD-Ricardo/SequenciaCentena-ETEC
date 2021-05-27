26727d1c471eb90c0d8f103ff49bdef1f4ad7c3a
RD-Ricardo <ricardodias169@gmail.com>
2021-05-27 15:04:35 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("--Sequencia Centena--");
+            Console.WriteLine("---------------------\n");
+            for(int i = 1 ;  i < 101 ; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
